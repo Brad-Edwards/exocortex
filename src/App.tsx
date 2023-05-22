15e0ee844dba42e9e7a20321cef2230012366ca4
Brad Edwards <brade@ensemble.com>
2023-05-22 10:32:40 -0700

    Add draft auth hook and slice
    Add draft header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Provider } from "react-redux";
 import store from "./store/store";
-import logo from './logo.svg'; // TODO: Add a logo
 import './App.css';
 import MainRouter from "./router/MainRouter";
 
